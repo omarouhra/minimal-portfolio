91c9be8cd940abb31a363be141c2244a01cb3620
omarouhra <omarmaroc26@gmail.com>
2022-08-05 18:46:33 +0100

    fix: SVG error

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,13 +4,13 @@ import Header from "./Header";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="bg-white dark:bg-[#2A2A2A] transition duration-500 px-5">
+    <div className="bg-white dark:bg-[#2A2A2A] transition duration-500 px-5">
       <div className='mx-auto max-w-2xl min-h-screen '>
         <Header />
         <main>{ children }</main>
         <Footer />
       </div>
-    </section>
+    </div>
   );
 }
 
