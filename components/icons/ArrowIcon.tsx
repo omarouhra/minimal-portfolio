ddafb70757e559d359fe41b1ff8e82c12fb5bda8
Omar Ouhra <hi@omarcode.me>
2022-08-06 09:33:54 +0100

    feat: starter template

--- components/icons/ArrowIcon.tsx
+++ components/icons/ArrowIcon.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Arrow() {
     return (
         <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg" >
-            <g clip-path="url(#clip0_1504_730)">
+            <g clipPath="url(#clip0_1504_730)">
                 <path d="M4.22876 4.72876V6.05812L9.49435 6.06283L3.75736 11.7998L4.70017 12.7426L10.4372 7.00564L10.4324 12.2712H11.7712V4.72876H4.22876Z" fillOpacity="0.87" className='fill-black dark:fill-white' />
             </g>
             <defs>
